fd027ea982626d8fe55ab28ca12ce472b65aa0c5
Joszef <jmadiguna@connect.ust.hk>
2020-06-19 16:33:46 +0700

    Split Original, HSV, and Mask

--- imgproc.js
+++ imgproc.js
@@ -9,20 +9,23 @@ imgElement.onload = function() {
     {
       let src = cv.imread(imgElement);
       let dst = new cv.Mat();
-      cv.cvtColor(src,src,cv.COLOR_RGB2HSV)
+      let hsv = new cv.Mat();
+      cv.cvtColor(src,hsv,cv.COLOR_RGB2HSV,0)
     
       //INSERT CODE HERE
     
-      let low = new cv.Mat(src.rows, src.cols, src.type(), [80,0,155,0]);
-      let high = new cv.Mat(src.rows, src.cols, src.type(), [140,255,255,255]);
-      cv.inRange(src,low,high,dst);
+      let low = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [80,0,155,0]);
+      let high = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [140,255,255,255]);
+      cv.inRange(hsv,low,high,dst);
 
       //Finish
       cv.imshow('canvasInput', src);
+      cv.imshow('outputHSV',hsv);
       cv.imshow('outputMask',dst);
     
       src.delete();
       dst.delete();
+      hsv.delete();
       low.delete();
       high.delete();
     }
