4d04816995cb564950435df492da77bc7c71fdbd
Joszef <jmadiguna@connect.ust.hk>
2020-06-19 16:25:51 +0700

    Successful HSV filtering

--- imgproc.js
+++ imgproc.js
@@ -5,18 +5,31 @@ inputElement.addEventListener('change', (e) => {
 }, false);
 
 imgElement.onload = function() {
-  let src = cv.imread(imgElement);
-  let dst = new cv.Mat();
-  cv.cvtColor(src,dst,cv.COLOR_RGB2HSV);
+  try
+    {
+      let src = cv.imread(imgElement);
+      let dst = new cv.Mat();
+      cv.cvtColor(src,src,cv.COLOR_RGB2HSV)
+    
+      //INSERT CODE HERE
+    
+      let low = new cv.Mat(src.rows, src.cols, src.type(), [80,0,155,0]);
+      let high = new cv.Mat(src.rows, src.cols, src.type(), [140,255,255,255]);
+      cv.inRange(src,low,high,dst);
 
-  //INSERT CODE HERE
-
-  //Finish
-  cv.imshow('canvasInput', src);
-  cv.imshow('canvasOutput',dst);
-
-  mat.delete();
-  dst.delete();
+      //Finish
+      cv.imshow('canvasInput', src);
+      cv.imshow('outputMask',dst);
+    
+      src.delete();
+      dst.delete();
+      low.delete();
+      high.delete();
+    }
+  catch(e)
+    {
+      console.error(e)
+    }
 };
 function onOpenCvReady() {
     document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
