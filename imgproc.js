722411617c74d5c14f38d6f4dd744b7e1496f759
Joszef <jmadiguna@connect.ust.hk>
2020-06-22 16:20:04 +0700

    Successful sorting of corners

--- imgproc.js
+++ imgproc.js
@@ -15,111 +15,112 @@ imgElement.onload = function() {
   try
     {
       //Make matrices needed to store images, show the process
-      let org = cv.imread(imgElement);
+        let org = cv.imread(imgElement);
       //Calculate the ratio
-      let width = 600;
-      let ratio = 0;
-        let newWidth = 0;
-        let newHeight = 0;
-        if ( org.cols > 0)
-          {
-            ratio = 600/org.cols;
-            newWidth = org.cols * ratio;
-            newHeight = org.rows * ratio;
-          }
-        // create new size matrix
-        let dsize = new cv.Size(newWidth,newHeight);
-      
-      let src = new cv.Mat();
+        let width = 600;
+        let ratio = 0;
+          let newWidth = 0;
+          let newHeight = 0;
+          if ( org.cols > 0)
+            {
+              ratio = 600/org.cols;
+              newWidth = org.cols * ratio;
+              newHeight = org.rows * ratio;
+            }
+          // create new size matrix
+          let dsize = new cv.Size(newWidth,newHeight);
+        
+        let src = new cv.Mat();
       //perform resizing with src as destination matrix
-      cv.resize(org,src,dsize,0,0,cv.INTER_AREA);
+        cv.resize(org,src,dsize,0,0,cv.INTER_AREA);
 
 
-      let hsv = new cv.Mat();
-      let mask = new cv.Mat();
-      let bound = new cv.Mat();
-      cv.cvtColor(src,hsv,cv.COLOR_RGB2HSV,0)
+        let hsv = new cv.Mat();
+        let mask = new cv.Mat();
+        let bound = new cv.Mat();
+        cv.cvtColor(src,hsv,cv.COLOR_RGB2HSV,0)
     
       //Filter blue color of given ID image
-      let low = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [80,0,155,0]);
-      let high = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [140,255,255,255]);
-      cv.inRange(hsv,low,high,mask);
+        let low = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [80,0,155,0]);
+        let high = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [140,255,255,255]);
+        cv.inRange(hsv,low,high,mask);
 
       //Get matrix object for contours
-      let contours = new cv.MatVector();
-      let hierarchy = new cv.Mat();
+        let contours = new cv.MatVector();
+        let hierarchy = new cv.Mat();
       //Find the contours of the image
-      cv.findContours(mask,contours,hierarchy,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE);
+        cv.findContours(mask,contours,hierarchy,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE);
       // Sort contours from largest to smallest
-      ArrCnts = [];
-      for(let i = 0; i < contours.size(); ++i)
-        {
-          let cnt = contours.get(i);
-          let area = cv.contourArea(cnt, false);
+        ArrCnts = [];
+        for(let i = 0; i < contours.size(); ++i)
+          {
+            let cnt = contours.get(i);
+            let area = cv.contourArea(cnt, false);
 
-          ArrCnts.push(new SortableContour(area,cnt));
-          
-        }
-        ArrCnts.sort(function(item1,item2){return item2.areaSize - item1.areaSize});
+            ArrCnts.push(new SortableContour(area,cnt));
+            
+          }
+          ArrCnts.sort(function(item1,item2){return item2.areaSize - item1.areaSize});
       //Extract the n largest contours
       //Extract the contour with 4 corners that is also the largest contour ( so far )
       
-      let biggestCnts = ArrCnts.slice(0,15);
-      let approxCnt = new cv.Mat();
+        let biggestCnts = ArrCnts.slice(0,15);
+        let approxCnt = new cv.Mat();
       //Get the largest contour and put it in approxCnt
-      for(let j = 0 ; j < biggestCnts.length ; ++j)
-        {
-          let peri = cv.arcLength(biggestCnts[j].contour,true);
-          cv.approxPolyDP(biggestCnts[j].contour,approxCnt,peri*0.015,true);
+        for(let j = 0 ; j < biggestCnts.length ; ++j)
+          {
+            let peri = cv.arcLength(biggestCnts[j].contour,true);
+            cv.approxPolyDP(biggestCnts[j].contour,approxCnt,peri*0.015,true);
 
-          if(approxCnt.rows==4)
-            {
-              console.log("Appropriate contour has been found !")
-              break;
-            }
-          else
-            {
-              console.log("APPROPRIATE CONTOUR NOT FOUND")
-              return;
-            }
-        }
+            if(approxCnt.rows==4)
+              {
+                console.log("Appropriate contour has been found !")
+                break;
+              }
+            else
+              {
+                console.error("APPROPRIATE CONTOUR NOT FOUND")
+                return;
+              }
+          }
       //Get the 4 corners
         cornerArray = []
         cornerArray.push(new cv.Point(approxCnt.data32S[0],approxCnt.data32S[1]));
         cornerArray.push(new cv.Point(approxCnt.data32S[2],approxCnt.data32S[3]));
         cornerArray.push(new cv.Point(approxCnt.data32S[4],approxCnt.data32S[5]));
-        cornerArray.push(new cv.Point(approxCnt.data32S[6],approxCnt.data32S[7]));
-
-        cornerArray.sort(function(a,b){return a.x-b.x});
-        console.log(cornerArray.length);
-        for(let i = 0 ; i < cornerArray.length ; ++i)
-          {
-              console.log("("+cornerArray[i].x+","+cornerArray[i].y+")");
-          }
-
+        cornerArray.push(new cv.Point(approxCnt.data32S[6],approxCnt.data32S[7]));  
       // //Reorder corners tl tr br bl
-      // cornerArray.sort(function(a,b){return b.y-a.y});
-      // for(let i = 0 ; i < cornerArray.length ; ++i)
-      //   {
-      //     console.log("This is a corner ("+cornerArray[i].x+","+cornerArray[i].y+")");
-      //   }
+        let sortedSum = cornerArray.sort(function(a,b){return((a.x+a.y)-(b.x+b.y))});
+        let tl = sortedSum[0];
+        let br = sortedSum[3];
+        let sortedDiff = cornerArray.sort(function(a,b){return((a.x-a.y)-(b.x-b.y))});
+        let tr = sortedDiff[3];
+        let bl = sortedDiff[0];
+        
+
+        console.log("The corners are: ");
+        console.log("("+tl.x+","+tl.y+")");
+        console.log("("+tr.x+","+tr.y+")");
+        console.log("("+bl.x+","+bl.y+")");
+        console.log("("+br.x+","+br.y+")");
+
       //Do perspective transform
       //Show the images in their respective canvases
-      cv.imshow('canvasInput', src);
-      cv.imshow('outputHSV',hsv);
-      cv.imshow('outputMask',mask);
+        cv.imshow('canvasInput', src);
+        cv.imshow('outputHSV',hsv);
+        cv.imshow('outputMask',mask);
       // cv.imshow('outputBound',bound);
 
       //Delete all matrices after used
-      org.delete();
-      src.delete();
-      mask.delete();
-      bound.delete();
-      low.delete();
-      high.delete();
-      contours.delete();
-      hierarchy.delete();
-      approxCnt.delete();
+        org.delete();
+        src.delete();
+        mask.delete();
+        bound.delete();
+        low.delete();
+        high.delete();
+        contours.delete();
+        hierarchy.delete();
+        approxCnt.delete();
       // boxCnt.delete();
     }
   catch(e)
